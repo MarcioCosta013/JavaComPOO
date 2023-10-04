c82f4f94ff5165ae4abdefc5cd8b76220595628e
Marcio Costa <marcio-costa38@hotmail.com>
2023-10-04 19:52:11 -0300

    aula pratica sobre visibilidade e como funciona cada uma delas.

---
+++ AprendendoPOO/src/aula03/Aula03.java
@@ -0,0 +1,17 @@
+package aula03;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+		Caneta c1 = new Caneta();
+		c1.modelo = "Bic";
+		c1.cor = "Vermelha";
+		c1.carga = 80;
+		//c1.ponta; -------> ponta é privada, só pode ser usada dentro da classe onde ela foi criada.
+		//c1.tampada; --------> o atributo é privado só pode ser modificado por um metodo que está na classe onde ele fo criado.
+		
+		c1.status();
+
+	}
+
+}
