07e1bd3cb87db7fda503d7805a8e3ce8be68dff9
Marcio Costa <marcio-costa38@hotmail.com>
2023-10-29 18:31:20 -0300

    Aula 09 - exercicio pratico - Projeto Livro/Leitor

---
+++ AprendendoPOO/src/aula09projetolivro/Aula09Main.java
@@ -0,0 +1,10 @@
+package aula09projetolivro;
+
+public class Aula09Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ AprendendoPOO/src/aula09projetolivro/Livro.java
@@ -0,0 +1,92 @@
+package aula09projetolivro;
+
+public class Livro implements Publicacao{
+	
+	//atributos
+	private String titulo, autor;
+	private int totPaginas, pagAtual;
+	private boolean aberto;
+	private Pessoa leitor;
+	
+	//construtor
+	public Livro(String titulo, String autor, int totPaginas, Pessoa leitor) {
+		this.titulo = titulo;
+		this.autor = autor;
+		this.aberto = false;
+		this.pagAtual = 0;
+		this.totPaginas = totPaginas;
+		this.leitor = leitor;
+	}
+	
+	//metodos especiais
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	
+	public Pessoa getLeitor() {
+		return leitor;
+	}
+	public void setLeitor(Pessoa leitor) {
+		this.leitor = leitor;
+	}
+	public int getTotPaginas() {
+		return totPaginas;
+	}
+	public void setTotPaginas(int totPaginas) {
+		this.totPaginas = totPaginas;
+	}
+	public int getPagAtual() {
+		return pagAtual;
+	}
+	public void setPagAtual(int pagAtual) {
+		this.pagAtual = pagAtual;
+	}
+	public boolean isAberto() {
+		return aberto;
+	}
+	public void setAberto(boolean aberto) {
+		this.aberto = aberto;
+	}
+	
+	//metodos
+	@Override
+	public String toString() {
+		return "Livro (titulo=" + titulo + ", autor=" + autor + ", totPaginas=" + totPaginas + ", pagAtual=" + pagAtual
+				+ ", aberto=" + aberto + ", leitor=" + leitor + ")";
+	}
+	
+	//metodos abstratos da interface
+	@Override
+	public void abrir() {	
+		this.aberto = true;
+	}
+
+	@Override
+	public void fechar() {
+		this.aberto = false;
+	}
+	@Override
+	public void folhear(int p) {
+		this.pagAtual = p;
+	}
+	@Override
+	public void avancarPag() {
+		this.pagAtual++;
+	}
+	@Override
+	public void voltarPag() {
+		this.pagAtual--;
+	}
+	
+	
+
+}
